fa9f28affc4b490b1377e15d56c29a846fd13464
clovis eyong nkem <cloviseyong@yahoo.com>
2023-05-17 21:21:40 +0700

    init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import reactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello Welcome to React!</h1>
+const myFirstElement = <h1>Hello Welcome to Reactssss!</h1>
 const root = reactDOM.createRoot(document.getElementById('root'));
 root.render(myFirstElement);
 
