f00f8cc38c7029a95ffa113a805a84ee619940ec
eyongaiden <nkem.clovis@gmail.com>
2023-06-13 00:52:44 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,139 @@
-import AboutMe from "./AboutMe";
-import Homepage from "./Homepage";
-import { Routes,Route, Link } from "react-router-dom";
+// import AboutMe from "./AboutMe";
+// import Homepage from "./Homepage";
+// import { Routes,Route, Link } from "react-router-dom";
 
-function App (){
+// import React from "react";
+import ReactPlayer from "react-player/youtube";
+
+const App = () => {
   return(
-    <div className ="App">
-      <nav className="nav">
-        <Link to="/" className="nav-item">Homepage</Link>
-        <Link to="/ about-me" className="nav-item">About Me</Link>
-        <Link to="/ about-me" className="nav-item">Contact</Link>
-      </nav>
-      <Routes>
-      <Route path="/" element={<Homepage/>}/>
-      <Route path="/about-me" element={<AboutMe/>}/>
-      </Routes>
+    <div>
+      <MyVideo/>
     </div>
   );
-}
+};
+
+  const MyVideo = ()=> {
+    return(
+      <ReactPlayer url ='https://www.youtube.com/watch?v=ysz56PUM-U'/>
+    );
+  };
+
+
 export default App;
+
+
+
+
+
+
+// // adding images
+// import rooftops from  './assets/logo512.png';
+
+
+// function App(){
+//   const randomImageUrl = "https://picsum.photos/400/265";
+//   return(
+//     <div>
+//       <h1>
+//         Task: add three images with some styling.
+//       </h1>
+//       <img 
+//       height ={200}
+//       src={rooftops}
+//       alt="An image of React"></img>
+
+//       <img 
+//       height={200}
+//       src={require ('./assets/logo512.png')}
+//       alt="An image of React"
+//       ></img>
+
+//       <img
+//       height={200}
+//       src={randomImageUrl}
+//       alt="Random Image"
+//       ></img>
+
+//     </div>
+//   )
+// }
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+// Conditional Statements in React
+
+// function App(){
+//   const time = new Date();
+//   const day = time.toLocaleString("en-us",{weekday:"long"});
+//   const morning = time.getHours() >= 6 && time.getHours() <= 12;
+//   let dayMessage;
+//   if(day.toLocaleLowerCase()=== "monday"){
+//     dayMessage = `Happy ${day}`;
+
+//   } else if (day.toLocaleLowerCase()==="tuesday"){
+//     dayMessage = `${day}, four days to go`;
+
+//   }else if (day.toLocaleLowerCase()==="wednesday"){
+//     dayMessage = `${day}, half way there`;
+
+//   }else if (day.toLocaleLowerCase()==="thurday"){
+//     dayMessage = `${day}, start planning the weekend`;
+
+//   }else if (day.toLocaleLowerCase()==="friday"){
+//     dayMessage = `Woo - hoo, the weekend is coming`;
+
+//   } else{
+//     dayMessage = " Stay calm and keep having fun";
+//   }
+
+//   return(
+//     <div className="App">
+//       <h1>
+//         {dayMessage}
+//       </h1>
+//       {morning? <h2> Have you had breakfast yet </h2> : ''}
+//     </div>
+//   );
+
+  
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+// function App (){
+//   return(
+//     <div className ="App">
+//       <nav className="nav">
+//         <Link to="/" className="nav-item">Homepage</Link>
+//         <Link to="/about-me" className="nav-item">About Me</Link>
+//         <Link to="/about-me" className="nav-item">Contact</Link>
+//       </nav>
+//       <Routes>
+//       <Route path="/" element={<Homepage/>}/>
+//       <Route path="/about-me" element={<AboutMe/>}/>
+//       </Routes>
+//     </div>
+//   );
+// }
+// export default App;
 
 
 
