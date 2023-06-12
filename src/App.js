4d7768bce07f8d9e39a157b63347418dafa8746f
eyongaiden <nkem.clovis@gmail.com>
2023-06-12 14:41:43 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 // import Card from "./Card";
 import Btn from "./Btn";
+import Child from "./Child";
 import "./App.css";
 
 
-function App() {
-  function handleClick(){
-    let randomNum = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNum);
-    let userInput = prompt('type a number');
-    alert (`Computer number: ${randomNum}, Your guess: ${userInput}`);
-  }
-  return (
-    <div>
-      <h1>Task: add a button and handle a cleck event </h1>
-      <button onClick={handleClick}>Guess the number between 1 and 3 </button>
-      <Btn />
-    </div>
-  );
-}
 
+
+function App(){
+  const date = new Date()
+
+  return(
+    <div>
+      <Child message = {date.toLocaleTimeString()}/>
+    </div>
+  )
+}
 export default App;
+
+
+
+
+
+
+// function App() {
+//   function handleClick(){
+//     let randomNum = Math.floor(Math.random() * 3) + 1;
+//     console.log(randomNum);
+//     let userInput = prompt('type a number');
+//     alert (`Computer number: ${randomNum}, Your guess: ${userInput}`);
+//   }
+//   return (
+//     <div>
+//       <h1>Task: add a button and handle a cleck event </h1>
+//       <button onClick={handleClick}>Guess the number between 1 and 3 </button>
+//       <Btn />
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
 
